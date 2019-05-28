64cea3ba47d55d56517933b5a01f153f3ef709af
silverbret1709 <cuongbuibk1709@gmail.com>
2019-05-28 07:53:50 +0700

    JWT

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,21 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
 
-Route::apiResource('/question', 'QuestionController');
-Route::apiResource('/category', 'CategoryController');
-Route::apiResource('/question/{question}/reply', 'ReplyController');
-Route::post('/like/{reply}', 'LikeController@likeIt');
-Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+//Route::apiResource('/question', 'QuestionController');
+//Route::apiResource('/category', 'CategoryController');
+//Route::apiResource('/question/{question}/reply', 'ReplyController');
+//Route::post('/like/{reply}', 'LikeController@likeIt');
+//Route::delete('/like/{reply}', 'LikeController@unLikeIt');
